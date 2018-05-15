bfa21bfa6450c91ab4e8c3bbc0b4619173ecd9d7
Yu Chen <yu.chen@live.ie>
2018-05-15 11:25:47 +0800

    优化

--- src/ios/UMShareWechat.h
+++ src/ios/UMShareWechat.h
@@ -1,12 +1,6 @@
 #import <Cordova/CDVPlugin.h>
 #import <UMShare/UMShare.h>
-#import <WXApi.h>
 
-@interface UMShareWechat : CDVPlugin<WXApiDelegate>
-
-@property (nonatomic, strong) NSString *currentCallbackId;
-@property (nonatomic, strong) NSString *appKey;
-
-- (void)auth:(CDVInvokedUrlCommand *)command;
+@interface UMShareWechat : CDVPlugin
 
 @end
--- src/ios/UMShareWechat.m
+++ src/ios/UMShareWechat.m
@@ -10,14 +10,10 @@
 
 - (void)finishLaunching:(NSNotification *)notification
 {
-    // Put here the code that should be on the AppDelegate.m
-    // 获取AppKey
+    // AppKey
     NSString *appKey = [[self.commandDelegate settings] objectForKey:@"um_share_wechat_key"];
-    NSLog(@"Wechat appKey: %@", appKey);
-    self.appKey = appKey;
-    // 获取AppSecret
+    // AppSecret
     NSString *appSecret = [[self.commandDelegate settings] objectForKey:@"um_share_wechat_secret"];
-    NSLog(@"Wechat appSecret: %@", appSecret);
     
     /*
      设置微信的appKey和appSecret
@@ -28,117 +24,6 @@
      * 移除相应平台的分享，如微信收藏
      */
     [[UMSocialManager defaultManager] removePlatformProviderWithPlatformTypes:@[@(UMSocialPlatformType_WechatFavorite)]];
-}
-
-- (void)auth:(CDVInvokedUrlCommand *)command
-{
-    if (TRUE)
-    {
-        // save the callback id
-        self.currentCallbackId = command.callbackId;
-    }
-    else
-    {
-        [self failWithCallbackID:command.callbackId withMessage:@"发送请求失败"];
-    }
-}
-
-- (void)successWithCallbackID:(NSString *)callbackID
-{
-    [self successWithCallbackID:callbackID withMessage:@"OK"];
-}
-
-- (void)successWithCallbackID:(NSString *)callbackID withMessage:(NSString *)message
-{
-    CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
-    [self.commandDelegate sendPluginResult:commandResult callbackId:callbackID];
-}
-
-- (void)failWithCallbackID:(NSString *)callbackID withMessage:(NSString *)message
-{
-    CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:message];
-    [self.commandDelegate sendPluginResult:commandResult callbackId:callbackID];
-}
-
-- (void)handleOpenURL:(NSNotification *)notification
-{
-    NSURL* url = [notification object];
-    
-    if ([url isKindOfClass:[NSURL class]] && [url.scheme isEqualToString:self.appKey])
-    {
-        [WXApi handleOpenURL:url delegate:self];
-    }
-}
-
-- (void)onReq:(BaseReq *)req
-{
-    NSLog(@"%@", req);
-}
-
-- (void)onResp:(BaseResp *)resp
-{
-    BOOL success = NO;
-    NSString *message = @"Unknown";
-    NSDictionary *response = nil;
-    
-    switch (resp.errCode)
-    {
-        case WXSuccess:
-            success = YES;
-            break;
-            
-        case WXErrCodeCommon:
-            message = @"普通错误";
-            break;
-            
-        case WXErrCodeUserCancel:
-            message = @"用户点击取消并返回";
-            break;
-            
-        case WXErrCodeSentFail:
-            message = @"发送失败";
-            break;
-            
-        case WXErrCodeAuthDeny:
-            message = @"授权失败";
-            break;
-            
-        case WXErrCodeUnsupport:
-            message = @"微信不支持";
-            break;
-            
-        default:
-            message = @"未知错误";
-    }
-    
-    if (success)
-    {
-        if ([resp isKindOfClass:[SendAuthResp class]])
-        {
-            // fix issue that lang and country could be nil for iPhone 6 which caused crash.
-            SendAuthResp* authResp = (SendAuthResp*)resp;
-            response = @{
-                         @"code": authResp.code != nil ? authResp.code : @"",
-                         @"state": authResp.state != nil ? authResp.state : @"",
-                         @"lang": authResp.lang != nil ? authResp.lang : @"",
-                         @"country": authResp.country != nil ? authResp.country : @"",
-                         };
-            
-            CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:response];
-            
-            [self.commandDelegate sendPluginResult:commandResult callbackId:self.currentCallbackId];
-        }
-        else
-        {
-            [self successWithCallbackID:self.currentCallbackId];
-        }
-    }
-    else
-    {
-        [self failWithCallbackID:self.currentCallbackId withMessage:message];
-    }
-    
-    self.currentCallbackId = nil;
 }
 
 @end
